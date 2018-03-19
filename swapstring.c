a90f0530ef69b147fa86c1b575343d9c1f6264a8
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-03-19 15:44:28 +0530

    Create swapstring.c
    print the swap string

---
+++ swapstring.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+void main()
+{
+
+    char temp,s[100];
+    int n,i;
+    gets(s);
+    //n=strlen(s);
+    i=0;
+    while (s[i]!='\0')
+    {
+        if(s[i+1]!='\0')
+        {
+            temp=s[i];
+            s[i]=s[i+1];
+            s[i+1]=temp;
+        }
+        i=i+2;
+
+
+
+}
+puts(s);
+}
